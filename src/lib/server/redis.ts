50f46656bc549723f15571df0ac3797c29a3c7e3
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-22 18:11:13 -0600

    no top level await

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -4,7 +4,7 @@ import { REDIS_URL } from '$env/static/private';
 const client = createClient({ url: REDIS_URL, socket: { keepAlive: false } }).setMaxListeners(0);
 
 client.on('error', error => console.error('ERR:REDIS:', error));
-await client.connect();
+client.connect();
 
 export const query = async (q: string) => await client.graph.query('code-build', q);
 
