66dbc0f4e0ef4355c3a306e80d1716342e1f93d2
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-23 09:15:02 -0600

    no warnings

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,7 +1,10 @@
 import { createClient } from "redis";
 import { REDIS_URL } from '$env/static/private';
+import { setMaxListeners } from 'events';
 
-const client = createClient({ url: REDIS_URL, socket: { keepAlive: false } }).setMaxListeners(0);
+const client = createClient({ url: REDIS_URL });
+
+setMaxListeners(0);
 
 client.on('error', error => console.error('ERR:REDIS:', error));
 client.connect();
