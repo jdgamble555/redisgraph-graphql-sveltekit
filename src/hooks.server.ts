29534e139880b13fa1f5a7950e90cd9919862f67
Jonathan Gamble <jdgamble555@gmail.com>
2023-01-22 18:28:20 -0600

    test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,6 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GoogleProvider from "@auth/core/providers/google";
-import { GOOGLE_ID, GOOGLE_SECRET, SECRET } from "$env/static/private";
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
 import type { Profile } from "@auth/core/types";
 import type { Provider } from "@auth/core/providers";
 import type { Handle } from "@sveltejs/kit";
@@ -11,7 +11,6 @@ export const handle = SvelteKitAuth({
       clientId: GOOGLE_ID,
       clientSecret: GOOGLE_SECRET
     }) as Provider<Profile>
-  ],
-  secret: SECRET
+  ]
 }) satisfies Handle;
 
