782a1c42fcac3ff10d9a56f504d92010cacb61a1
Jonathan Gamble <jdgamble555@gmail.com>
2023-03-12 22:23:56 -0500

    test

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -1,5 +1,6 @@
 import { createClient } from "redis";
 import { REDIS_URL } from '$env/static/private';
+import { browser } from "$app/environment";
 
 const client = createClient({ url: REDIS_URL });
 
@@ -7,3 +8,7 @@ client.on('error', error => console.error('ERR:REDIS:', error));
 client.connect();
 
 export const query = async (q: string) => await client.graph.query('test-graphs', q);
+
+if (browser) {
+    console.log("huh");
+}
