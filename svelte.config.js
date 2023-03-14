48f6de3e17e8a0a3c40dabde5a64b88102d65453
Jonathan Gamble <jdgamble555@gmail.com>
2023-03-13 21:16:04 -0500

    cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,9 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			edge: true
-		})
+		adapter: adapter()
 	}
 };
 
